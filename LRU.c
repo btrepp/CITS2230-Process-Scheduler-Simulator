07def89e3ba76619a41b95feb12d62f5cf4e27b8
20263557 <20263557@gp05.(none)>
2011-10-20 15:14:46 +0800

    Added Psuedo code for the mem paging

---
+++ LRU.c
@@ -0,0 +1,10 @@
+
+// LRU algorithm
+// mode: swaping the blocks or removal of the pages from a blocks
+// memoryblock: define which mem to remove or swap from(physical(50) or virtual(200)
+// pagecounter: Checks if the removed pages is sufficient for the process that requires the space
+
+void LRU(mode, memoryblock, pagecounter)
+{
+  // Perform the LRU algorithm
+}
