d57fc2bccb5229fbb3f87811293e5a76d1683e8a
Beau Trepp <beautrepp@gmail.com>
2011-10-28 00:50:53 +0800

    now dumps html memory with colour coded ram

--- htmloutput.c
+++ htmloutput.c
@@ -1,50 +1,79 @@
 #include "htmloutput.h"
 
-void scriptJS(FILE* out);
+void scriptJSHeader(FILE* out);
 
 void header(FILE* out){
 	if(out==NULL) return ;
 	fprintf(out, "<html><head><title>Memory Output</title>\n");
-	fprintf(out, "<script \"text/javascript\">\n");
-	scriptJS(out);
+	fprintf(out, "<script type=\"text/javascript\" \n "
+		     "src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js\">\n"
+		     "</script>\n");
+	fprintf(out, "<script type=\"text/javascript\">\n");
+	scriptJSHeader(out);
 }
 
-void scriptJS(FILE* out){
-	if(out==NULL) return ;
+void scriptJSHeader(FILE* out){
 	fprintf(out, "function memElement( jobname){\n"
-		"   return { \n"
+	"   if($(\"style:contains('.\" + jobname + \"')\").length < 1){ \n"
+   		"       $(\"<style type='text/css'>.\"+jobname+\"{\" +  \n"
+        	"       \"background-color:\"+Math.round(0xffffff * Math.random()).toString(16)+\";\"+ \n"
+        	"       \"float:left;\"+ \n"
+        	"       \"width:60px; } </style>\").appendTo(\"head\"); \n"
+   		"   }\n"
+	"   return { \n"
 		"      jobname:jobname\n"
 		"   }\n"
 		"}\n");
 	fprintf(out,"var all = [];\n");
+	fprintf(out,"var clock =0;\n");
+	
+}
 
-	fprintf(out, "function printClock(clock){\n"
-//		"   document.getElementsByTagName('BODY')[0].innerHTML = '';\n"
-		"   document.write(\"<p>\" + clock + \"</p>\");\n"
-		"   document.write(\"<form>\"); \n"
-		"   alert('<input type=\"button\" value=\"Next Clock!\" onclick=\" printClock(0);\" />');\n"
-		"   document.write('<input type=\"button\" value=\"Next Clock!\" onclick=\" printClock('+clock+1')\" />');\n"
-		"   document.write(\" </form>\");\n"
-		"   for( elem in all[clock]) { \n"
-		"   document.write(\"<p>\" + elem.jobname + \"</p>\");"
-		"   }\n"
+
+void scriptJSFooter(FILE* out){
+	if(out==NULL) return ;
+	fprintf(out, "<script type=\"text/javascript\"> \n ");
+	fprintf(out, "$('#increment').click(function (e) { \n"
+       		     "      e.preventDefault(); \n"
+        	     "      clock++; \n"
+    		     "      printClock(); \n"
+		     "}); \n");
+	fprintf(out, "$('#decrement').click(function (e) { \n"
+       		     "      e.preventDefault(); \n"
+        	     "      clock--; \n"
+    		     "      printClock(); \n"
+		     "}); \n");
+
+	fprintf(out, "function printClock(){\n"
+		"   $('#content').empty();\n "
+		"   $('#clockcount').html(clock); \n"
+		"   jQuery.each(all[clock], function(i, val) {\n"
+		"         $('#content').append(\"<div>\");\n"
+		"         $('#content').append(\"<div class=\"+val.jobname+\">\" +val.jobname+\"</div>\");\n"
+		"         $('#content').append(\"</div>\");\n"
+		"   });\n"
 		"}\n");
+	fprintf(out, "</script> \n ");
 	
 }
 
 void footer(FILE* out){
 	if(out==NULL) return ;
 	fprintf(out, "document.write(\"<p>\" + Date() + \"</p>\");");
-
-	fprintf(out, "printClock(0);");
-
-
 	fprintf(out, "</script>\n");
 	fprintf(out, "<body>\n");
+	fprintf(out, "<div>\n");
+	fprintf(out, "<div id=\"clockcount\"></div>\n");
+	fprintf(out, "<input type=\"button\" value=\"Previous\" id=\"decrement\"/>\n");	
+	fprintf(out, "<input type=\"button\" value=\"Next\" id=\"increment\"/>\n");
+	fprintf(out, "</div>\n");
+	fprintf(out, "<div id=\"content\"></div>\n");
+	scriptJSFooter(out);
 	fprintf(out,"</body></html>\n");
 }
 
 void memToJavascriptArray(FILE* out, Memory* mem){
+	//return;
 	if(out==NULL) return ;
 	fprintf(out,"var memspace = [];\n");
 	for(int i=0;i<mem->number_of_pages; i++){
